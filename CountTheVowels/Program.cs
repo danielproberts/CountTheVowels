22463c7a46e14df4c7c8e5191505e29269974a47
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-10-25 17:42:08 -0400

    Add project files.

---
+++ CountTheVowels/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CountTheVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of vowels in 'Abacus': ");
+            CountVowels("Abacus");
+            Console.WriteLine();
+
+            Console.Write("Number of vowels in 'AmErIcA': ");
+            CountVowels("AmErIcA");
+            Console.WriteLine();
+
+            Console.Write("Number of vowels in 'ttttttt': ");
+            CountVowels("ttttttt");
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+
+        static void CountVowels(string input)
+        {
+            Regex rx = new Regex("[aeiou]", RegexOptions.IgnoreCase);
+            MatchCollection matches = rx.Matches(input);
+            Console.Write(matches.Count);
+        }
+    }
+}
